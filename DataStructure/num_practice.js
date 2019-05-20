dfb6d84966cc2dcac13b6efe1dbc2ec21998059c
milimlee <milimlee0@gmail.com>
2019-05-19 22:57:23 -0400

    Update num_practice.js

--- DataStructure/num_practice.js
+++ DataStructure/num_practice.js
@@ -63,6 +63,30 @@ function reverseArray(a) {
 }
 
 
+function hourglassSum(arr) {
+    let maxI = 3;
+    let maxJ = 3;
+    let Tsum = -64;
+   
+    for (let i = 0; i <= maxI; i++) {
+        for (let j = 0; j <= maxJ; j++) {
+            // sum for the top of hourglass
+            let sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+
+            // add the middle of hourglass
+            sum += arr[i + 1][j + 1];
+
+            // add the bottom of hourglass
+            sum += arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]
+
+            if (sum > Tsum)
+                Tsum = sum ;
+        }
+    }
+    return Tsum;
+}
+
+
 
 
 
