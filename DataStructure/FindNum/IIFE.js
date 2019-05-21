26fe6582656f51bc0f16302bc580a5257305d62a
milimlee <milimlee0@gmail.com>
2019-05-21 01:41:55 -0400

    Update IIFE.js

--- DataStructure/FindNum/IIFE.js
+++ DataStructure/FindNum/IIFE.js
@@ -12,3 +12,21 @@
     if(true) console.log('Hello World!');
     
  })();
+
+
+
+function createButtons() {
+   for (var i = 1; i <= 5; i++) {
+     var body = document.getElementsByTagName("BODY")[0];
+     var button = document.createElement("BUTTON");
+     button.innerHTML = 'Button ' + i;
+     (function(num){
+       button.onclick = function() {
+          alert('This is button ' + num);
+       }
+     })(i);
+     body.appendChild(button);
+   }
+}
+ 
+createButtons();
